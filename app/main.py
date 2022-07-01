106fba619f3d0540c9f34047d5ef026ab449430c
anish-gbt <anish@greatbear.tech>
2022-07-01 22:39:06 +0545

    Project Setup

---
+++ app/main.py
@@ -0,0 +1,7 @@
+def main():
+    # import and initialize BERT model
+    from models.model import create_model
+    create_bert()
+
+if __name__ == '__main__':
+    main()
---
+++ app/models/bert.py
@@ -0,0 +1,6 @@
+from bertopic import BERTopic
+
+def create_bert():
+    print('Creating BERT model')
+    topic_model = BERTopic(language="english", calculate_probabilities=True, verbose=True)
+    print('Bert Topic Model = ', topic_model)
